8daee69e56f342b4fdb75155192e9c899182baa3
Abel Gutierrez <abel4@msn.com>
2016-03-02 16:57:15 -0600

    Initial commit

---
+++ Assets/Game.cpp
@@ -0,0 +1,44 @@
+/****************************************************************************************** 
+ *  Chili DirectX Framework Version 11.12.17                                              * 
+ *  Game.cpp                                                                              *
+ *  Copyright 2011 PlanetChili.net                                                        *
+ *                                                                                        *
+ *  This file is part of The Chili DirectX Framework.                                     *
+ *                                                                                        *
+ *  The Chili DirectX Framework is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by                  *
+ *  the Free Software Foundation, either version 3 of the License, or                     *
+ *  (at your option) any later version.                                                   *
+ *                                                                                        *
+ *  The Chili DirectX Framework is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                        *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                         *
+ *  GNU General Public License for more details.                                          *
+ *                                                                                        *
+ *  You should have received a copy of the GNU General Public License                     *
+ *  along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+ ******************************************************************************************/
+#include "Game.h"
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+Game::Game( HWND hWnd,const KeyboardServer& kServer,const MouseServer& mServer )
+:   gfx( hWnd ),
+    kbd( kServer ),
+    audio( hWnd ),
+    mouse( mServer )
+{}
+
+Game::~Game()
+{}
+
+void Game::Go()
+{
+    gfx.BeginFrame();
+    ComposeFrame();
+    gfx.EndFrame();
+}
+
+void Game::ComposeFrame()
+{}
