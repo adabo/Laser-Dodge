f081833cf29344139056f9580ce412541dab075b
Abel Gutierrez <abel4@msn.com>
2016-03-15 14:18:47 -0500

    Fixed vector<Laser> not iterating

--- Assets/Physics.cpp
+++ Assets/Physics.cpp
@@ -18,12 +18,34 @@ void Physics::CollisionCheck(Player &ThisPlayer, std::vector<Enemy> &Enemies,
            (int)ThisPlayer.GetY() < (int)enemy.GetY() + enemy.GetHeight())
        {}
     }
-    for (auto &laser : Lasers)
+    // int p = 0;
+    //if (Lasers.size() >= 3)
+    // for (auto &laser : Lasers)
+    if (Lasers.size())
     {
-       if ((int)ThisPlayer.GetX() + ThisPlayer.GetWidth() > (int)laser.GetX() &&
-           (int)ThisPlayer.GetX() < (int) laser.GetX() + laser.GetWidth() &&
-           (int)ThisPlayer.GetY() + ThisPlayer.GetHeight() > (int)laser.GetY() &&
-           (int)ThisPlayer.GetY() < (int)laser.GetY() + laser.GetHeight())
-       {}
+        for (int i = Lasers.size() - 1; i >= 0; --i)
+        {
+           if ((int)ThisPlayer.GetX() + ThisPlayer.GetWidth() > (int)Lasers[i].GetX() &&
+               (int)ThisPlayer.GetX() < (int) Lasers[i].GetX() + Lasers[i].GetWidth() &&
+               (int)ThisPlayer.GetY() + ThisPlayer.GetHeight() > (int)Lasers[i].GetY() &&
+               (int)ThisPlayer.GetY() < (int)Lasers[i].GetY() + Lasers[i].GetHeight() ||
+               (Lasers[i].x <= 1.0f || Lasers[i].x >= 798.0f || Lasers[i].y <= 1.0f || Lasers[i].y >=589.0f))
+           {
+                // Try adding lasers to erase in array
+                // then delete them outside this for loop
+               Lasers.erase(Lasers.begin() + i);
+           }
+        }
     }
 }
+
+// int i = 0;
+// for (auto &laser : Lasers)
+// {
+//     if (laser.x >= SCREENWIDTH || laser.x <= 0 ||
+//         laser.y >= SCREENHEIGHT || laser.y <= 0)
+//     {
+//         Lasers.erase(Lasers.begin() + i);
+//         ++i
+//     }
+// }
